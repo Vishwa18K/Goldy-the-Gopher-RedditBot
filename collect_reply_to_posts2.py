7db7e2ac9249b10a6923cdabac9cf84611475cc9
Vishwa <vishwa.krishnan1232@gmail.com>
2025-05-24 15:02:03 -0500

    collect_reply_to_posts2.py

--- collect_reply_to_posts2.py
+++ collect_reply_to_posts2.py
@@ -21,27 +21,27 @@ reddit = praw.Reddit(
 
 
 
-# raw_weekly_posts = []
-# weekly_posts = []
+raw_weekly_posts = []
+weekly_posts = []
 
-# weekly_posts.clear()
+weekly_posts.clear()
 
-# for submission in reddit.subreddit("uofmn").new(limit=None):
+for submission in reddit.subreddit("uofmn").new(limit=None):
    
 
-#     clean_title = ''.join([c for c in submission.title if c in string.printable])
-#     clean_selftext = ''.join([c for c in submission.selftext if c in string.printable])
+    clean_title = ''.join([c for c in submission.title if c in string.printable])
+    clean_selftext = ''.join([c for c in submission.selftext if c in string.printable])
 
-#     combined_line = f"{clean_title}: {clean_selftext}"
-#     weekly_posts.append(combined_line)
+    combined_line = f"{clean_title}: {clean_selftext}"
+    weekly_posts.append(combined_line)
 
 
-# file = open("weekly_posts.csv", "w", newline="", encoding="utf-8")
-# writer = csv.writer(file) 
-# writer.writerow(["Titles"])  
-# for line in weekly_posts:
-#     writer.writerow([line])
-# file.close()
+file = open("weekly_posts.csv", "w", newline="", encoding="utf-8")
+writer = csv.writer(file) 
+writer.writerow(["Titles"])  
+for line in weekly_posts:
+    writer.writerow([line])
+file.close()
 
 for submission in reddit.subreddit("uofmn").search("undergrad graduation + google workspace access"):
         if submission.title.lower() == "undergrad graduation + google workspace access":
